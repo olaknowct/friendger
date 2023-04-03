1e0378f65b7e1760d451b521d0b06557e2ca0d86
Christopher Olano <olanochristopher@gmail.com>
2023-04-03 20:50:20 +0800

    update main component

--- src/component/main/main.component.jsx
+++ src/component/main/main.component.jsx
@@ -1,15 +1,17 @@
 import './main.styles.scss';
-import Section from './section/section.component';
+import Messenger from './messenger/messenger.component';
 import { UserProvider } from '../../context/user.context';
+import { MessageContext } from '../../context/message.context';
+import { useContext } from 'react';
 // import { ReactComponent as Phone } from '../../svgs/phone.svg';
 // import { ReactComponent as VideoCamera } from '../../svgs/video-camera.svg';
 
 const Main = () => {
+  const { popMessage } = useContext(MessageContext);
+  console.log(popMessage);
   return (
     <main className='main'>
-      <UserProvider>
-        <Section />
-      </UserProvider>
+      <UserProvider>{popMessage ? <h1>test</h1> : <Messenger />}</UserProvider>
     </main>
   );
 };
