b6c5f3f72acf0683a147b6246b8b03d8242a1d24
Christopher Olano <olanochristopher@gmail.com>
2023-04-03 20:51:28 +0800

    uppdate component name: section to messenger

---
+++ src/component/main/messenger/messenger.component.jsx
@@ -0,0 +1,19 @@
+import './messenger.styles.scss';
+
+import Form from '../form/form.component';
+import FriendList from '../friend-list/friend-list.component';
+
+const Messenger = () => {
+  return (
+    <div className='messenger-container'>
+      <section className='main-header '>
+        <h1 className='title'>Friend List</h1>
+        <Form />
+
+        <FriendList />
+      </section>
+    </div>
+  );
+};
+
+export default Messenger;
