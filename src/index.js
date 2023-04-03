dd8c08065a7e9d1e3b2d40fe779409f8cafdc80e
Christopher Olano <olanochristopher@gmail.com>
2023-04-03 20:49:55 +0800

    update placing message provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { MessageProvider } from './context/message.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MessageProvider>
+      <App />
+    </MessageProvider>
   </React.StrictMode>
 );
 
