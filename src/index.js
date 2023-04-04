102b8c7b778f1213c3aea26032ffdaa8a9a5f0c4
Christopher Olano <olanochristopher@gmail.com>
2023-04-04 13:47:42 +0800

    update inbox context provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { MessageProvider } from './context/message.context';
+import { InboxProvider } from './context/inbox.context';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MessageProvider>
+    <InboxProvider>
       <App />
-    </MessageProvider>
+    </InboxProvider>
   </React.StrictMode>
 );
 
