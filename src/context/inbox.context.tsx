4a3471faa6f737b17b96dbbc92c8d857aaa671ec
Christopher Olano <olanochristopher@gmail.com>
2023-04-26 02:23:16 +0800

    typing inbox context

---
+++ src/context/inbox.context.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState } from 'react';
+
+type Message = {
+  myMessage: string;
+  message: string;
+  date: string;
+};
+
+type Inbox = {
+  popInbox: boolean;
+  inbox: Message[];
+  setPopInbox: (popInbox: boolean) => void;
+  setInbox: (inbox: Message[]) => void;
+};
+
+export const InboxContext = createContext<Inbox>({
+  popInbox: false,
+  inbox: [],
+  setPopInbox: () => {},
+  setInbox: () => {},
+});
+
+export const InboxProvider = ({ children }: { children: React.ReactNode }) => {
+  const [popInbox, setPopInbox] = useState<boolean>(false);
+  const [inbox, setInbox] = useState<Message[]>([]);
+
+  const value: Inbox = { popInbox, inbox, setInbox, setPopInbox };
+
+  return <InboxContext.Provider value={value}>{children}</InboxContext.Provider>;
+};
